9318b3aa40b076860e1d9a0c382651a89eed5279
Lukasz Spyra <lukasz.spyrka@gmail.com>
2020-02-08 20:44:55 +0100

    Task6 answer added

---
+++ Question6.java
@@ -0,0 +1,14 @@
+public class Question6 {
+
+    public static void main(String[] args){
+
+        private int dayOfTheWeek = 6;
+        private boolean holiday = false;
+
+        if (dayOfTheWeek > 0 && dayOfTheWeek < 6 && !holiday){
+            System.out.println("Wake up at 7:00")
+        } else {
+            System.out.println("Sleep in!")
+        }
+    }
+}
